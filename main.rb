e7fd93b4096034886314181f762e965477a37c5d
Lionel <info@am-i-live.com>
2015-04-24 18:08:32 -0400

    lionel updated information

--- main.rb
+++ main.rb
@@ -2,5 +2,25 @@ require "sinatra"
 require "mandrill"
 
 get '/' do
-	erb :index
+	erb :home
+end
+
+
+get '/' do
+	erb :about
+end
+
+
+get '/' do
+	erb :amiStudio
+end
+
+
+get '/' do
+	erb :amiFilm
+end
+
+
+get '/' do
+	erb :contact
 end
